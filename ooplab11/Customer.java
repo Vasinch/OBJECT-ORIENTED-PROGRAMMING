f5407b6425503a69e9a5e2a74b31dcfd60ebffe3
Vasin chantaragon <139621838+Vasinch@users.noreply.github.com>
2025-03-06 23:58:18 +0700

    Add files via upload

---
+++ ooplab11/Customer.java
@@ -0,0 +1,59 @@
+package ooplab11;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private CheckingAccount acct;
+    
+    public Customer(){
+        this("", "", null);
+    }
+    
+    public Customer(String firstName,String lastName){
+        this(firstName, lastName, null);
+    }
+    
+    public Customer(String firstName,String lastName, CheckingAccount acct){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.acct = acct;
+    }
+    
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    
+    public String getFirstName(){
+        return this.firstName;
+    }
+    
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    
+    public String getLastName(){
+        return this.lastName;
+    }
+    
+    public void setAcct(CheckingAccount acct){
+        this.acct = acct;
+    }
+    
+    public CheckingAccount getAcct(){
+        return this.acct;
+    }
+    
+    @Override
+    public String toString(){
+        if(getAcct() == null){
+            return this.firstName+" "+this.lastName+" doesn’t have account.";
+        }
+        else{
+            return "The "+this.firstName+" account has "+acct.getBalance()+" baht and "+acct.getCredit()+" credits.";
+        }
+    }
+    
+    public boolean equals(Customer c){
+        return c.getFirstName().equals(getFirstName()) && c.getLastName().equals(getLastName());
+    } 
+}
---
+++ ooplab11/WithdrawException.java
@@ -0,0 +1,12 @@
+package ooplab11;
+
+public class WithdrawException extends Exception{
+    
+    public WithdrawException(){
+        super();
+    }
+    
+    public WithdrawException(String s){
+        super(s);
+    }
+}
