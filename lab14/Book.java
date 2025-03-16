59d202e7311985c31d336b2ba05e98fe7c9b38e0
Vasin chantaragon <139621838+Vasinch@users.noreply.github.com>
2025-03-16 21:43:43 +0700

    Add files via upload

---
+++ lab14/Book.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author vasin
+ */
+public class Book implements Serializable{
+    private String name;
+    private double price;
+    private String type;
+    
+    public Book(){
+        this("",0.0,"");
+    }
+    
+    public Book(String name,double price,String type){
+        this.name = name;
+        this.price = price;
+        this.type = type;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setPrice(double price){
+        this.price = price;
+    }
+    
+    public double getPrice(){
+        return this.price;
+    }
+    
+    public void setType(String type){
+        this.type = type;
+    }
+    
+    public String getType(){
+        return this.type;
+    }
+}
+
